73c56377c0259cb516c2047eb7cb5eb185974301
jasmer529 <12210651@mail.sustech.edu.cn>
2025-04-08 11:21:25 +0800

    lab8 jacoco

--- docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
+++ docs-core/src/test/java/com/sismics/docs/core/dao/jpa/TestJpa.java
@@ -8,6 +8,16 @@ import com.sismics.docs.core.util.authentication.InternalAuthenticationHandler;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.sismics.docs.core.dao.UserDao;
+import com.sismics.docs.core.dao.criteria.UserCriteria;
+import com.sismics.docs.core.util.jpa.SortCriteria;
+import com.sismics.docs.core.model.jpa.User;
+
+import com.sismics.docs.core.dao.dto.UserDto;
+
+import java.util.List;
+
+
 /**
  * Tests the persistance layer.
  * 
@@ -34,4 +44,25 @@ public class TestJpa extends BaseTransactionalTest {
         userDao.delete("testJpa", user.getId());
         TransactionUtil.commit();
     }
+
+    @Test
+    public void testFindByCriteria_searchOnly() throws Exception {
+        UserDao userDao = new UserDao();
+
+        User user = createUser("findByCriteriaUser");
+        TransactionUtil.commit();
+
+        UserCriteria criteria = new UserCriteria();
+        criteria.setSearch("findByCriteriaUser");
+        //criteria.setGroupId()
+        SortCriteria sortCriteria = new SortCriteria(0,true);
+
+        List<UserDto> users = userDao.findByCriteria(criteria, sortCriteria);
+
+        Assert.assertFalse(users.isEmpty());
+        UserDto userDto = users.get(0);
+        Assert.assertEquals("findByCriteriaUser", userDto.getUsername());
+        Assert.assertEquals("toto@docs.com", userDto.getEmail());
+    }
+
 }
